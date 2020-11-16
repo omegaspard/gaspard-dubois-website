80ddb8364a2ce03e8e0d909cd32e96b884adcf74
omegaspard <gdiopdub@gmail.com>
2020-11-15 16:29:26 -0800

    Complete deployment.sh, fix some typos.

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,8 @@
-# Put this on a CI/CD workflow.
-# Deploying the application.
-# You need to set the variable IP_MACHINE, USER, PASSWORD
+# TODO Put this on a CI/CD workflow.
+# Script to deploy  the application.
+# You need to set the variables IP_MACHINE, USER, PASSWORD and SECRET_KEY
 
 sbt dist
+sshpass -p $PASSWORD scp -rp target/universal/gaspard-dubois-website-1.0-SNAPSHOT.zip  $USER@$IP_MACHINE:/home/ubuntu/applications/zips
+sshpass -p $PASSWORD ssh $USER@$IP_MACHINE "cd applications; unzip -o zips/gaspard-dubois-website-1.0-SNAPSHOT.zip; cd gaspard-dubois-website-1.0-SNAPSHOT; kill -9 \$(cat RUNNING_PID); rm RUNNING_PID; nohup sudo bin/gaspard-dubois-website -Dplay.http.secret.key=\"${SECRET_KEY}\" -Dhttp.port=80 &"
+
