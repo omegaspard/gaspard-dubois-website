e07e50dd5af49bbff1317e2634f61e8f89488a81
omegaspard <gdiopdub@gmail.com>
2020-11-15 16:44:22 -0800

    Replace user name by variable.

--- deployment.sh
+++ deployment.sh
@@ -3,6 +3,6 @@
 # You need to set the variables IP_MACHINE, USER, PASSWORD and SECRET_KEY
 
 sbt dist
-sshpass -p $PASSWORD scp -rp target/universal/gaspard-dubois-website-1.0-SNAPSHOT.zip  $USER@$IP_MACHINE:/home/ubuntu/applications/zips
-sshpass -p $PASSWORD ssh $USER@$IP_MACHINE "cd applications; unzip -o zips/gaspard-dubois-website-1.0-SNAPSHOT.zip; cd gaspard-dubois-website-1.0-SNAPSHOT; kill -9 \$(cat RUNNING_PID); rm RUNNING_PID; nohup sudo bin/gaspard-dubois-website -Dplay.http.secret.key=\"${SECRET_KEY}\" -Dhttp.port=80 &"
+sshpass -p $PASSWORD scp -rp target/universal/gaspard-dubois-website-1.0-SNAPSHOT.zip  $USER@$IP_MACHINE:/home/$USER/applications/zips
+sshpass -p $PASSWORD ssh $USER@$IP_MACHINE "cd applications; unzip -o zips/gaspard-dubois-website-1.0-SNAPSHOT.zip; cd gaspard-dubois-website-1.0-SNAPSHOT; sudo kill -9 \$(cat RUNNING_PID); rm RUNNING_PID; nohup sudo bin/gaspard-dubois-website -Dplay.http.secret.key=\"${SECRET_KEY}\" -Dhttp.port=80 &"
 
